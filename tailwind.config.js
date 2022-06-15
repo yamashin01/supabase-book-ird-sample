987b1068b9e75d5e86f30a9e7414fe4daa22aacc
yamada <shinya.yamada@benext-corp.com>
2022-06-15 09:19:54 +0900

    ✨ add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
